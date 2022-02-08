e9d7ac44a4fc5c90e95ab672bc2f233976f73b13
MeiraGraf <73069348+MeiraGraf@users.noreply.github.com>
2022-02-08 17:49:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,45 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { getNpmData } from "./utils";
-import { NpmPackageTree } from "./NpmPackageTree";
-import { PackageNodeInfo } from "./Types";
-
-const input = document.querySelector("input");
-const log = document.getElementById("values");
-
-
+import React, { useState } from 'react';
+import { getNpmData } from './utils';
+import { NpmPackageTree } from './NpmPackageTree';
+import { PackageNodeInfo } from './Types';
 export default App;
 
- function App() {
-  const [packageName, setPackageName] = useState<string>("Enter package name");
-  const [packageDetails, setPackageDetails] = useState<PackageNodeInfo>();
+function App() {
+    const [packageName, setPackageName] = useState<string>();
+    const [packageDetails, setPackageDetails] = useState<PackageNodeInfo>();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <label>
-          Package name:
-          <input
-            title={"input package name"}
-            value={packageName}
-            onChange={(inputText) => setPackageName(inputText.target.value)}
-            style={{float: "left"}}
-          />
-        </label>
-        <button
-          style={{ margin: "10px", float: "left" }}
-          onClick={async () => {
-            const npmData = await getNpmData(packageName);
-            setPackageDetails(npmData);
-          }}
-          title={"get npm data"}
-          >get npm data</button>
-      
-
-        {packageDetails && <NpmPackageTree rootNode={packageDetails} packageName ={packageName}/>}
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header>
+                <label>
+                    Enter Package name:
+                    <input
+                        title={'input package name'}
+                        value={packageName}
+                        onChange={(inputText) =>
+                            setPackageName(inputText.target.value)
+                        }
+                        style={{ marginLeft: '4px' }}
+                    />
+                </label>
+                <button
+                    style={{ margin: '10px' }}
+                    onClick={async () => {
+                        const npmData = await getNpmData(packageName);
+                        setPackageDetails(npmData);
+                    }}
+                >
+                    Get NPM Package info
+                </button>
+                {packageDetails && (
+                    <NpmPackageTree
+                        rootNode={packageDetails}
+                        packageName={packageName}
+                    />
+                )}
+            </header>
+        </div>
+    );
 }
 
 
